3224aea34e7cfcd995ff61e0d01511c4b4cf8af0
Scott Bragg <scott.bragg@gmail.com>
2021-04-22 19:59:06 -0400

    diff sort
    sorting and diffing two files to output lines that don't exist in both files.

---
+++ diffit.sh
@@ -0,0 +1,2 @@
+ ## scriptlet -- this needs debugging... throws errors sometimes but all the time.
+ diff -i --new-line-format="" --unchanged-line-format="" <(sort list_1.lst) <(sort list_2.lst) | tee stuff_in_list_1_not_in_list_2.lst
